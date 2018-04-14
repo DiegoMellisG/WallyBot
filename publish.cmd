ab898edd8680c498275594049e36be4d9073c2bd
DiegoMellisG <diego.mellis@usach.cl>
2018-04-14 10:13:55 -0300

    First commit men

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.LuisBot.sln -p:DeployOnBuild=true -p:PublishProfile=wallybot-Web-Deploy.pubxml -p:Password=tr0iP6CXdw2C1Y4ejC1vCq2Ry5lNMK4aopQiGsSvXx7f5Dw4b4wTvcBucDiJ
+
